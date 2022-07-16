8de77cada1ed01deab1eaa39e2786bd541899a9a
Eason Ma <a705193483@gmail.com>
2022-07-17 00:29:41 +0930

    commit section product

--- project_one/src/App.js
+++ project_one/src/App.js
@@ -6,27 +6,47 @@ const Header = () => {
       <h2 className="left-heading">Introducing iWatch</h2>
       <ul className="right-heading">
         <li className="right-nav">
-          <a href="#product"></a>Product
+          <a href="#product">Product</a>
         </li>
         <li className="right-nav">
-          <a href="#feature"></a>Feature
+          <a href="#feature">Feature</a>
         </li>
         <li className="right-nav">
-          <a href="#review"></a>Review
+          <a href="#review">Review</a>
         </li>
         <li className="right-nav">
-          <a href="#contact"></a>Contact
+          <a href="#contact">Contact</a>
         </li>
       </ul>
     </div>
   );
 };
 
-const Page = () => {
+const Introduction = () => {
   return (
-    <div className="page">
-    <h1>THE FUTURE IS HERE</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <div className="introduction">
+      <h1>THE FUTURE IS HERE</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <br></br>
+      <a href="https://www.youtube.com/" className="demo-button">See a Demo</a>
+      <br></br>
+      <img src="apple-watch.png" alt="apple-watch"></img>
+    </div>
+  )
+}
+
+const Product = () => {
+  return (
+    <div className="product" id="product">
+      <h2>The product</h2>
+      <p>Introduce the product here. A small description about what it is and how it helps the user. You can also add some photos below.</p>
+      <div className="clock-animation-block">
+        <button></button>
+        <div class="apple-watch-2">
+          <img src="apple-watch-2.png" alt="apple-watch-2"></img>
+        </div>
+        <button></button>
+      </div>
     </div>
   )
 }
@@ -35,7 +55,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Page />
+      <Introduction />
+      <Product />
     </div>
   );
 }
